01e410ad781191c7d82affe41caa9a90402cd81a
Thais Mathias <thais-mathias@hotmail.com>
2020-08-30 23:31:05 -0300

    Adicionando função findById.

--- src/main/java/com/instathagram/repository/PostagemRepository.java
+++ src/main/java/com/instathagram/repository/PostagemRepository.java
@@ -4,6 +4,9 @@ import com.instathagram.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+    Optional<Postagem> findById(Long postagemId);
 }
